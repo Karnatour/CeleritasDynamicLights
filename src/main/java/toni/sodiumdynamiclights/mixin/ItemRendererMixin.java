261e1f8c5a78d224f7f9e1a82317a97ada4a04bb
Vojtěch Šokala <vojtasokala@gmail.com>
2026-06-14 14:04:23 +0200

    Make held item lit if light source is on ground

--- src/main/java/toni/sodiumdynamiclights/mixin/ItemRendererMixin.java
+++ src/main/java/toni/sodiumdynamiclights/mixin/ItemRendererMixin.java
@@ -1,8 +1,10 @@
 package toni.sodiumdynamiclights.mixin;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,7 +29,17 @@ public class ItemRendererMixin {
     private int modifyLightValueArg(int par2) {
         int mainHand = SodiumDynamicLights.getLuminanceFromItemStack(itemStackMainHand, SodiumDynamicLights.isEyeSubmergedInFluid(this.mc.player));
         int secondHand = SodiumDynamicLights.getLuminanceFromItemStack(itemStackOffHand, SodiumDynamicLights.isEyeSubmergedInFluid(this.mc.player));
-        return Math.max(mainHand, secondHand);
+
+        AbstractClientPlayer player = this.mc.player;
+        BlockPos pos = new BlockPos(
+                player.posX,
+                player.posY + player.getEyeHeight(),
+                player.posZ
+        );
+
+        int dynamicLight = (int) SodiumDynamicLights.get().getDynamicLightLevel(pos);
+
+        return Math.max(Math.max(mainHand, secondHand), dynamicLight);
     }
 }
 
